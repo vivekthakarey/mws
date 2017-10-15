cef5ec21bf2c6a24df7811aaef3842c412bee82a
VIVEK-PC\VIVEK <javageek88@gmail.com>
2017-10-15 16:19:17 -0400

    Initial spring boot structure

---
+++ mpv/src/main/java/com/vivek/mpv/MpvApplication.java
@@ -0,0 +1,12 @@
+package com.vivek.mpv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpvApplication.class, args);
+	}
+}
